cc97893ce70273e89099562b1750459763a2e9c4
Reno D Hermawan <reno.d.hermawan@gmail.com>
2021-03-31 11:37:23 +0700

    add polindrome method

--- main.go
+++ main.go
@@ -3,14 +3,39 @@ package main
 import "fmt"
 
 func main() {
+	var input Word
 	fmt.Print("Input a word: ")
+	fmt.Scan(&input.word)
 
+	polindrome, err := input.polindromeDeterminer()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(polindrome)
 }
 
 type Word struct {
-	input string
+	word string
 }
 
-func (w Word) polindromDeterminer() (string, error) {
-	return "", nil
+func (w Word) polindromeDeterminer() (string, error) {
+	//error handler for only one character
+	if len(w.word) <= 1 {
+		return "", fmt.Errorf("No word has been enter")
+	}
+
+	//reversing input
+	output := []rune(w.word)
+	for i, j := 0, len(output)-1; i < j; i, j = i+1, j-1 {
+		output[i], output[j] = output[j], output[i]
+	}
+
+	//condition for polindrome or not
+	if string(output) == w.word {
+		return string("Polindrome: " + string(output)), nil
+	} else {
+		return string("Not Polindrome: " + string(output)), nil
+	}
 }
