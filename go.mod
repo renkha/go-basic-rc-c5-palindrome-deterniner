f3c1dab8272ea1d136c566955d0b51321de917f0
Reno D Hermawan <reno.d.hermawan@gmail.com>
2021-03-31 08:50:48 +0700

    first push

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/renkha/go-basic-rc-c5-palindrome-deterniner
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Input a word: ")
+
+}
+
+type Word struct {
+	input string
+}
+
+func (w Word) polindromDeterminer() (string, error) {
+	return "", nil
+}
